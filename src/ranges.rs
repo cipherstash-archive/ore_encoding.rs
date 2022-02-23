fea9da25a7103dc046910e5082d875fab3a9af10
James Sadler <james@cipherstash.com>
2022-02-23 17:22:02 +1100

    Add tests for range encoding
    These are fairly basic so far and we need to cover some more edge
    conditions.

--- src/ranges.rs
+++ src/ranges.rs
@@ -40,3 +40,48 @@ pub fn encode_gte<T>(value: OrePlaintext<T>) -> OreRange<T> where T: OrePlaintex
 pub fn encode_eq<T>(value: OrePlaintext<T>) -> OreRange<T> where T: OrePlaintextOps {
     OreRange{ min: value, max: value }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::OrePlaintext;
+
+    #[test]
+    fn test_encode_eq() -> () {
+        let pt = OrePlaintext(0u64);
+        let range = encode_eq(pt);
+        assert_eq!(range.min, range.max)
+    }
+
+    #[test]
+    fn test_encode_lt() -> () {
+        let pt = OrePlaintext(100u64);
+        let range = encode_lt(pt);
+        assert_eq!(range.min, OrePlaintext(0u64));
+        assert_eq!(range.max, OrePlaintext(99u64))
+    }
+
+    #[test]
+    fn test_encode_lte() -> () {
+        let pt = OrePlaintext(100u64);
+        let range = encode_lte(pt);
+        assert_eq!(range.min, OrePlaintext(0u64));
+        assert_eq!(range.max, OrePlaintext(100u64))
+    }
+
+    #[test]
+    fn test_encode_gt() -> () {
+        let pt = OrePlaintext(100u64);
+        let range = encode_gt(pt);
+        assert_eq!(range.min, OrePlaintext(101u64));
+        assert_eq!(range.max, OrePlaintext(std::u64::MAX))
+    }
+
+    #[test]
+    fn test_encode_gte() -> () {
+        let pt = OrePlaintext(100u64);
+        let range = encode_gte(pt);
+        assert_eq!(range.min, OrePlaintext(100u64));
+        assert_eq!(range.max, OrePlaintext(std::u64::MAX))
+    }
+}
